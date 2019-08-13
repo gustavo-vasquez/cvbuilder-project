2ed4dad6ca5dde3b8ce7dd37ab5047bbd76ff740
Gustavo Vasquez <vasquez.mgustavo@gmail.com>
2019-08-13 20:16:01 -0300

    Proyecto separado en 3 capas (no definitivo).

---
+++ CVBuilder/CVBuilder.Domain/Class1.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CVBuilder.Domain
+{
+    public class Class1
+    {
+    }
+}
